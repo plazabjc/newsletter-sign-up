51f96df350e3f2c45b2f3c37441b84197ec34169
Juan Plaza <plazabjc@gmail.com>
2023-11-14 12:03:33 -0500

    Add card

--- newsletter/pages/index.js
+++ newsletter/pages/index.js
@@ -3,100 +3,43 @@ import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
 export default function Home() {
+  const items = [
+    { text: "Product discovery and building what matters" },
+    { text: "Measuring to ensure updates are a success" },
+    { text: "And much more" },
+  ];
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Stay updated!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className={styles.title}>
-          Read <Link href="/posts/first-post">this page!</Link>
-        </h1>
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className={styles.main}>
+        <div className={styles.gridContent}>
+          <div className={styles.grid}>
+            <div className={styles.card}>
+              <h3>Stay updated!</h3>
+              <p>Join 60,000+ product managers receiving monthly updates on:</p>
+              <ul className={styles.imageList}>
+                {items.map((item, index) => (
+                  <li key={index}>
+                    <img src="/images/icon-list.svg" alt="Common Image" />
+                    {item.text}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className={styles.card}>
+              <img
+                src="/images/illustration-sign-up-desktop.svg"
+                alt="Common Image"
+              />
+            </div>
+          </div>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
       <style jsx global>{`
         html,
         body {
